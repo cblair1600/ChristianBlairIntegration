6ad043b33ee4519786f399c9afdb5f8530be8f78
crblair2297 <crblair2297@L438R3S1.STUDENT.ad.fgcu.edu>
2018-02-01 18:49:18 -0500

    Update 2

--- ChristianBlairIntegration/src/Main.java
+++ ChristianBlairIntegration/src/Main.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 // Christian Blair
 // My project that integrates the skills 
 // I have learned thus far
-
+// sysout ctrl+space
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-	    Dog spot = new Dog();
+		System.out.println("Welcome to my Project!");
+		Dog spot = new Dog();
 	    spot.size = 30;
 	    spot.breed = "German Shepherd";
 	    System.out.println(spot.breed);
 	    Dog spuds = new Dog();
 	    spuds.size = 40;
+	    System.out.println("");
 	}
 
 }
