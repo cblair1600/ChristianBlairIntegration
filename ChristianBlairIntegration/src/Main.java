f9798580208379533ac41191580449cf15b6b3b2
crblair2297 <crblair2297@L438R3S1.STUDENT.ad.fgcu.edu>
2018-02-01 19:00:57 -0500

    Update 3

--- ChristianBlairIntegration/src/Main.java
+++ ChristianBlairIntegration/src/Main.java
@@ -1,26 +1,17 @@
 import java.util.Scanner;
 
 // Christian Blair
-// My project that integrates the skills 
-// I have learned thus far
-// sysout ctrl+space
+// My project displays the skills I have learned
+//It will be a program for a quiz type game about the NBA
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to my Project!");
-		Dog spot = new Dog();
-	    spot.size = 30;
-	    spot.breed = "German Shepherd";
-	    System.out.println(spot.breed);
-	    Dog spuds = new Dog();
-	    spuds.size = 40;
-	    System.out.println("");
-	}
+  public static void main(String[] args) {
+    // sysout ctrl + space
+    // ctrl + Shift + F to auto format everything
+    System.out.println("Welcome to my Project!");
+   
+  
 
-}
-
-class Dog {
-		double size;
-		String breed;
-	
-}
+  }
+  }
