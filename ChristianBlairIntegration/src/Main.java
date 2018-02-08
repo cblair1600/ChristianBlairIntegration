841862a990ae0e6a44d514856f6a95755e2c2cf8
Christian <Christian@10.0.0.159>
2018-02-08 16:31:23 -0500

    Added variable descriptions, variables, operators, strings, arithmetic

--- ChristianBlairIntegration/src/Main.java
+++ ChristianBlairIntegration/src/Main.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-// Christian Blair
-// My project displays the skills I have learned
-//It will be a program for a quiz type game about the NBA
-
+/* Christian Blair
+*My project displays the skills I have learned
+* and how they work 
+*/
 public class Main {
 
   public static void main(String[] args) {
@@ -11,7 +11,28 @@ public class Main {
     // ctrl + Shift + F to auto format everything
     System.out.println("Welcome to my Project!");
    
-  
-
+    /* Variables in Java store information that contain 
+     * values and are assigned with their own data types
+     */
+    
+    /* Java's built in data types, or primitive types include:
+     * Int : 32 bit integer value      | Short: 16 bit integer value   
+     * Char: any unicode character, '' | Boolean: True or false value
+     * Float: single precision numbers | Double: Double precision numbers
+     * Byte: 8 bit integer, can limit  | Long: 64 bit integer, wider than int  
+     */
+    System.out.println("Simple Arithmetic Examples");
+    int x = 5;
+    double y = 7.83;
+        System.out.println("The value of X is " + x);
+        System.out.println("X + Y will result in a double and is");
+        System.out.println(x+y);
+        System.out.println("What is the Y value?");
+        //Add User Response
+        
+        // Boolean response for Is X greater than Y?
+        System.out.println("    ");
+        System.out.println("Is X greater than or equal to Y?");
+        System.out.println(x >= y);
   }
   }
